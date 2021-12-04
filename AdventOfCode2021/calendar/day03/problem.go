2af989aa4fe21a964c66eb8279cced24727ae509
Jari Duyvejonck <jari.duyvejonck@outlook.com>
2021-12-04 17:44:54 +0100

    move from int64 to int

--- AdventOfCode2021/calendar/day03/problem.go
+++ AdventOfCode2021/calendar/day03/problem.go
@@ -2,6 +2,7 @@ package day03
 
 import (
 	"AdventOfCode2021/util/aocstring"
+	"errors"
 	"log"
 	"strconv"
 	"strings"
@@ -27,8 +28,8 @@ func solvePart1(diagnosticReportString string) int {
 
 	diagnosticNumbers := convertBinaryStrings(diagnosticReport)
 
-	gammaRate := CalculateGammaRate(diagnosticNumbers, int64(len(diagnosticReport[0])))
-	epsilonRate := CalculateEpsilonRate(diagnosticNumbers, int64(len(diagnosticReport[0])))
+	gammaRate := CalculateGammaRate(diagnosticNumbers, int(len(diagnosticReport[0])))
+	epsilonRate := CalculateEpsilonRate(diagnosticNumbers, int(len(diagnosticReport[0])))
 
 	//log.Printf("gamma rate: '%d' (binary '%b')", gammaRate, gammaRate)
 	//log.Printf("epsilon rate: '%d' (binary '%b')", epsilonRate, epsilonRate)
@@ -36,64 +37,151 @@ func solvePart1(diagnosticReportString string) int {
 	return gammaRate * epsilonRate
 }
 
-func convertBinaryStrings(binaryStrings []string) []int64 {
-	numbers := make([]int64, len(binaryStrings))
+func convertBinaryStrings(binaryStrings []string) []int {
+	numbers := make([]int, len(binaryStrings))
 
 	for i, binaryString := range binaryStrings {
 		if number, err := strconv.ParseInt(binaryString, 2, 64); err != nil {
 			log.Fatal(err)
 		} else {
-			numbers[i] = number
+			numbers[i] = int(number)
 		}
 	}
 
 	return numbers
 }
 
-func CalculateGammaRate(numbers []int64, size int64) int {
-	gammaRateFunc := func(counter1, counter0 int) bool { return counter1 > counter0 }
-
-	return CalculateValue(numbers, size, gammaRateFunc)
+func CalculateGammaRate(numbers []int, size int) int {
+	return CalculateValue(numbers, size, isCounter1LargerCounter0)
 }
 
-func CalculateEpsilonRate(numbers []int64, size int64) int {
-	epsilonRateFunc := func(counter1, counter0 int) bool { return counter1 < counter0 }
-
-	return CalculateValue(numbers, size, epsilonRateFunc)
+func CalculateEpsilonRate(numbers []int, size int) int {
+	return CalculateValue(numbers, size, isCounter0LargerCounter1)
 }
 
-func CalculateValue(numbers []int64, size int64, f func(int, int) bool) int {
+func isCounter1LargerCounter0(counter0, counter1 int) int {
+	if counter1 > counter0 {
+		return 1
+	}
+	return 0
+}
+
+func isCounter0LargerCounter1(counter0, counter1 int) int {
+	if counter1 < counter0 {
+		return 1
+	}
+
+	return 0
+}
+
+func isCounter1LargerOrEqualCounter0(counter0, counter1 int) int {
+	if counter1 > counter0 || counter0 == counter1 {
+		return 1
+	}
+	return 0
+}
+
+func isCounter0LargerOrEqualCounter1(counter0, counter1 int) int {
+	if counter1 > counter0 || counter0 == counter1 {
+		return 0
+	}
+
+	return 1
+}
+
+func CalculateValue(numbers []int, size int, f func(int, int) int) int {
 	endValue := 0
 
 	for i := size - 1; i >= 0; i-- {
 		endValue = endValue << 1
 
-		counter1 := 0
-		counter0 := 0
-		m := int64(1 << i)
+		counter0, counter1 := counter0And1(numbers, i)
 
-		for _, number := range numbers {
-			r := number & m
-
-			if r == m {
-				counter1++
-			} else {
-				counter0++
-			}
-		}
-
-		if f(counter1, counter0) {
-			endValue++
-		}
+		endValue += f(counter0, counter1)
 	}
 
 	return endValue
+}
+
+func calculateOxygenGeneratorRating(numbers []int, size int) int {
+	return calculateComplexValue(numbers, size, isCounter1LargerOrEqualCounter0)
+}
+
+func calculateCo2ScrubberRating(numbers []int, size int) int {
+	return calculateComplexValue(numbers, size, isCounter0LargerOrEqualCounter1)
+}
+
+func calculateComplexValue(numbers []int, size int, f func(int, int) int) int {
+	if len(numbers) == 1 {
+		return int(numbers[0])
+	} else if size < 0 {
+		log.Fatalf("no metric number found")
+	}
+
+	filteredNumbers, err := filterNumbers(numbers, size, f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return calculateComplexValue(filteredNumbers, size-1, f)
+}
+
+func filterNumbers(numbers []int, bitPos int, f func(int, int) int) ([]int, error) {
+	m := int(1 << (bitPos - 1))
+
+	numbers0 := make([]int, 0)
+	numbers1 := make([]int, 0)
+
+	for _, number := range numbers {
+		r := number & m
+
+		if r == m {
+			numbers1 = append(numbers1, number)
+		} else {
+			numbers0 = append(numbers0, number)
+		}
+	}
+
+	switch f(len(numbers0), len(numbers1)) {
+	case 0:
+		return numbers0, nil
+	case 1:
+		return numbers1, nil
+	default:
+		return nil, errors.New("can't filter number array")
+	}
+}
+
+func counter0And1(numbers []int, bitPos int) (counter0, counter1 int) {
+	m := int(1 << bitPos)
+
+	for _, number := range numbers {
+		r := number & m
+
+		if r == m {
+			counter1++
+		} else {
+			counter0++
+		}
+	}
+
+	return
 }
 
 func part2(problem string) {
 	log.Printf("Day 03 part 2: Binary Diagnostic: %d", solvePart2(problem))
 }
 
-func solvePart2(diagnosticString string) int {
-	return 0
+func solvePart2(diagnosticReportString string) int {
+	diagnosticReport := strings.Split(diagnosticReportString, "\n")
+
+	diagnosticNumbers := convertBinaryStrings(diagnosticReport)
+
+	oxygenGeneratorRating := calculateOxygenGeneratorRating(diagnosticNumbers, int(len(diagnosticReport[0])))
+	co2ScrubberRating := calculateCo2ScrubberRating(diagnosticNumbers, int(len(diagnosticReport[0])))
+
+	//log.Printf("gamma rate: '%d' (binary '%b')", oxygenGeneratorRating, oxygenGeneratorRating)
+	//log.Printf("epsilon rate: '%d' (binary '%b')", co2ScrubberRating, co2ScrubberRating)
+
+	return oxygenGeneratorRating * co2ScrubberRating
 }
--- AdventOfCode2021/calendar/day03/problem_test.go
+++ AdventOfCode2021/calendar/day03/problem_test.go
@@ -1,12 +1,28 @@
 package day03
 
 import (
+	"AdventOfCode2021/util/aocstring"
 	"testing"
 )
 
+var testNumbers = []int{
+	0b00100,
+	0b11110,
+	0b10110,
+	0b10111,
+	0b10101,
+	0b01111,
+	0b00111,
+	0b11100,
+	0b10000,
+	0b11001,
+	0b00010,
+	0b01010,
+}
+
 func TestConvertBinaryStrings(t *testing.T) {
 	binaryStrings := []string{"10110", "00100", "11110"}
-	numbers := []int64{22, 4, 30}
+	numbers := []int{22, 4, 30}
 
 	result := convertBinaryStrings(binaryStrings)
 
@@ -21,24 +37,32 @@ func TestConvertBinaryStrings(t *testing.T) {
 	}
 }
 
-func TestCalculateGammaRate(t *testing.T) {
-	numbers := []int64{
-		int64(0b00100),
-		int64(0b11110),
-		int64(0b10110),
-		int64(0b10111),
-		int64(0b10101),
-		int64(0b01111),
-		int64(0b00111),
-		int64(0b11100),
-		int64(0b10000),
-		int64(0b11001),
-		int64(0b00010),
-		int64(0b01010),
+func TestCounter1And0(t *testing.T) {
+	tests := []struct {
+		bitPos   int
+		counter0 int
+		counter1 int
+	}{
+		{4, 5, 7},
 	}
 
+	for _, test := range tests {
+		c0, c1 := counter0And1(testNumbers, test.bitPos)
+
+		if c0 != test.counter0 {
+			t.Fatalf("counter0 wanted '%d' but got '%d'", test.counter0, c0)
+		}
+
+		if c1 != test.counter1 {
+			t.Fatalf("counter1 wanted '%d' but got '%d'", test.counter1, c1)
+		}
+	}
+
+}
+
+func TestCalculateGammaRate(t *testing.T) {
 	want := 22
-	got := CalculateGammaRate(numbers, 5)
+	got := CalculateGammaRate(testNumbers, 5)
 
 	if want != got {
 		t.Fatalf("Wrong gamma rate, wanted '%d' (binary '%b') got '%d' (binary '%b')", want, want, got, got)
@@ -46,30 +70,15 @@ func TestCalculateGammaRate(t *testing.T) {
 }
 
 func TestCalculateEpsilonRate(t *testing.T) {
-	numbers := []int64{
-		int64(0b00100),
-		int64(0b11110),
-		int64(0b10110),
-		int64(0b10111),
-		int64(0b10101),
-		int64(0b01111),
-		int64(0b00111),
-		int64(0b11100),
-		int64(0b10000),
-		int64(0b11001),
-		int64(0b00010),
-		int64(0b01010),
-	}
-
 	want := 9
-	got := CalculateEpsilonRate(numbers, 5)
+	got := CalculateEpsilonRate(testNumbers, 5)
 
 	if want != got {
 		t.Fatalf("Wrong gamma rate, wanted '%d' (binary '%b') got '%d' (binary '%b')", want, want, got, got)
 	}
 }
 
-func TestSolvePart1(t *testing.T) {
+func TestSolvePart1_TestProblem(t *testing.T) {
 	problem := `00100
 11110
 10110
@@ -88,5 +97,67 @@ func TestSolvePart1(t *testing.T) {
 
 	if want != got {
 		t.Fatalf("SolvePart1, want '%d', got '%d'", want, got)
+	}
+}
+
+func TestSolvePart1(t *testing.T) {
+	problem := aocstring.ReadProblemString("input")
+
+	want := 2498354
+	got := solvePart1(problem)
+
+	if want != got {
+		t.Fatalf("SolvePart1, want '%d', got '%d'", want, got)
+	}
+}
+
+func TestCalculateOxygenGeneratorRating(t *testing.T) {
+	want := 23
+	got := calculateOxygenGeneratorRating(testNumbers, 5)
+
+	if want != got {
+		t.Fatalf("OxygenGeneratorRating wanted '%d' (binary '%05b') but got '%d' (binary '%05b')", want, want, got, got)
+	}
+}
+
+func TestCalculateCo2ScrubberRating(t *testing.T) {
+	want := 10
+	got := calculateCo2ScrubberRating(testNumbers, 5)
+
+	if want != got {
+		t.Fatalf("OxygenGeneratorRating wanted '%d' (binary '%05b') but got '%d' (binary '%05b')", want, want, got, got)
+	}
+}
+
+func TestSolvePart2(t *testing.T) {
+	problem := aocstring.ReadProblemString("input")
+
+	want := 3277956
+	got := solvePart2(problem)
+
+	if want != got {
+		t.Fatalf("SolvePart2, want '%d', got '%d'", want, got)
+	}
+}
+
+func TestSolvePart2_TestProblem(t *testing.T) {
+	problem := `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`
+
+	want := 230
+	got := solvePart2(problem)
+
+	if want != got {
+		t.Fatalf("SolvePart2, want '%d', got '%d'", want, got)
 	}
 }
