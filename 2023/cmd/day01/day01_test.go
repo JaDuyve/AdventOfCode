05c33a090ba83bde808c3fb023281f362688677f
Jari Duyvejonck <jari.duyvejonck@outlook.com>
2023-12-03 21:29:12 +0100

    remove testify dependency

--- 2023/cmd/day01/day01_test.go
+++ 2023/cmd/day01/day01_test.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestDay01(t *testing.T) {
-	asrt := assert.New(t)
 	input := "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
 
 	t.Run("part 1", func(t *testing.T) {
 		expected := 142
 		actual := part1(input)
 
-		asrt.Equal(expected, actual)
+		if actual != expected {
+			t.Errorf("expected: %d, actual: %d", expected, actual)
+		}
 	})
 
 	input = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
@@ -22,6 +21,8 @@ func TestDay01(t *testing.T) {
 		expected := 281
 		actual := part2(input)
 
-		asrt.Equal(expected, actual)
+		if actual != expected {
+			t.Errorf("expected: %d, actual: %d", expected, actual)
+		}
 	})
 }
